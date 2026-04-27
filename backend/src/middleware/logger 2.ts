640a95f83162d5455b7acf35cc002ecf58a4abbf
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-04-27 11:36:14 +0200

    tog bort dubletter

--- backend/src/middleware/logger 2.ts
+++
@@ -1,7 +0,0 @@
-// src/middleware/logger.ts
-import { Request, Response, NextFunction } from 'express';
-
-module.exports = (req: Request, _res: Response, next: NextFunction) => {
-  console.log(`${req.method} ${req.path}`);
-  next();
-};
