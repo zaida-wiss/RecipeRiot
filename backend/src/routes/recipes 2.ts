30b964ac13752252727153b75e378622c39848a7
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-04-27 11:24:32 +0200

    importväg uppdaterad

---
+++ backend/src/routes/recipes 2.ts
@@ -0,0 +1,13 @@
+// src/routes/recipes.ts
+import { Router } from 'express';
+const recipesController = require('../controllers/recipesController');
+
+const router = Router();
+
+router.get('/', recipesController.getAllRecipes);
+router.get('/:id', recipesController.getRecipeById);
+router.post('/', recipesController.createRecipe);
+router.patch('/:id', recipesController.updateRecipe);
+router.delete('/:id', recipesController.deleteRecipe);
+
+module.exports = router;
