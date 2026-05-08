e4fbf7a437568201697269f8a6c1dea016acbb2c
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-05-08 12:39:09 +0200

    uppdaterade users Routen så att den inte blandar import och recuire, och därmed inte heller exporterar på olika sätt

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,12 +1,13 @@
 // src/routes/usersController.ts
 import { Router } from "express";
-const usersController = require("../controllers/usersController");
+import { getAllUsers, getUserById, createUser, updateUser, deleteUser } from "../controllers/usersController"
 
 const router = Router();
 
-router.get("/", usersController.getAllUsers);
-router.get("/:id", usersController.getUserById);
-router.post("/", usersController.createUser);
-router.put("/:id", usersController.updateUser);
-router.delete("/:id", usersController.deleteMovie);
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.post("/", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
+export default router;
