247cec35c38aab47873e4c6eeb420fc1ea176366
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-05-08 12:31:51 +0200

    skapade routres för user

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,7 +1,12 @@
-const { Users } = require("../models/Users");
+// src/routes/usersController.ts
+import { Router } from "express";
+const usersController = require("../controllers/usersController");
 
-//Här skall usersController importeras in ---->
 const router = Router();
 
-router.post("/",
-}
+router.get("/", usersController.getAllUsers);
+router.get("/:id", usersController.getUserById);
+router.post("/", usersController.createUser);
+router.put("/:id", usersController.updateUser);
+router.delete("/:id", usersController.deleteMovie);
+
