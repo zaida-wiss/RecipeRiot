fc079e082a8c0ec6112cc4682f8ab8dd29ffe3b2
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-05-10 01:51:05 +0200

    uppdaterade userModel

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -2,45 +2,43 @@ import mongoose, { Schema } from "mongoose";
 import { UserDocument } from "../types/userType";
 
 const usersSchema = new Schema<UserDocument>(
-{
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+      required: true,
+    },
+
+    passwordHash: {
+      type: String,
+      required: true,
+      select: false,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
   },
-
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-
-  role: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user",
-    required: true,
-  },
-
-  passwordHash: {
-    type: String,
-    required: true,
-    select: false,
-  },
-
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-
-},
-
-{ timestamps: true }
+  { timestamps: true }
 );
 
 
-export const userModel = mongoose.model<UserDocument>("User", usersSchema);
+export const User = mongoose.model<UserDocument>("User", usersSchema);
