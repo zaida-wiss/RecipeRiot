e2553eb2750e05d976d8d30677e05f6ee877d2c1
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-05-10 23:45:51 +0200

    testmappen påbörjad, installationspaket hämtat, test setupen skriven

---
+++ backend/src/__tests__/setup.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongoServer: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  await mongoose.connect(mongoServer.getUri());
+});
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const collection of Object.values(collections)) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
