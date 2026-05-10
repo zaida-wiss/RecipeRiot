749f8d89f1c233a927bfcf4c4b9b142f93a2b7b3
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-05-10 18:35:18 +0200

    updateUserObject klar

--- backend/src/controllers/usersController.ts
+++ backend/src/controllers/usersController.ts
@@ -86,46 +86,40 @@ const updateUserObject = async (
 ) => {
   try {
     const id = req.params.id;
-    const { username, email, password} = req. body;
+    const user = await User.findById(id);
 
-    if(!hasRequiredUserFields(username, email, password)) {
-      return res.status(400).json({
-        error: {
-          message: "username, email och password krävs.",
-          status: 400,
-        },
-      });
-    }
-
-    const updatedUser = await User.findByIdAndUpdate(
-      id,
-      {
-        username,
-        email,
-        passwordHash: `hashed-${password}`,
-      },
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
-
-    if (!updatedUser) {
+    if (!user) {
       return res.status(404).json({
         error: {
-          message: "Användaren hittades inte",
+          message: "Användaren hittades inte.",
           status: 404,
         },
       });
     }
 
+    const { username, email, password } = req.body;
+
+    if (!hasRequiredUserFields(username, email, password)) {
+      return res.status(400).json({
+        error: {
+          message: "username, email och lösenord krävs.",
+          status: 400,
+        },
+      });
+    }
+
+    user.username = username;
+    user.email = email;
+    user.passwordHash = `hashed-${password}`;
+
+    const updatedUser = await user.save();
+
     return res.json(updatedUser);
   } catch (error) {
     return next(error);
   }
-
-
 };
+
 
 // const updateUserObject = (req: Request, res: Response) => {
 //   const id = Number(req.params.id);
