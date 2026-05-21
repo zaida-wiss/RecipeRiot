7c7847621371feca371bc79231a1858a65c8443f
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-05-21 16:51:28 +0200

    fixade stavfel

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,10 +4,10 @@ import app from './app';
 import { env } from "./config/env";
 import { connectToDatabase } from './config/database';
 
-export const startServer() = async(): Promise<void> => {
+export const startServer = async(): Promise<void> => {
   await connectToDatabase();
   app.listen(env.PORT, () => {
-    console.log(`Servern lyssnar på http://localhost:${PORT}`);
+    console.log(`Servern lyssnar på http://localhost:${env.PORT}`);
   });
 }
 
