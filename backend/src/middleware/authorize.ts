d19200e14a2d2010e053369bf07ce14609a28f3b
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-05-25 11:41:08 +0200

    uppdaterade recipeControllern med delete funktionen så att endast admin behövrigheter kan ta bort andras recept

--- backend/src/middleware/authorize.ts
+++ backend/src/middleware/authorize.ts
@@ -2,7 +2,7 @@ import  { Request, Response, NextFunction } from "express";
 import { ForbiddenError, UnauthorizedError } from "../errors/AppError.js";
 import type { UserRole } from "../types/index.js";
 
-export const authorizeRole = (...allowedRoles: UserRole[]) => {
+export const authorizeRoles = (...allowedRoles: UserRole[]) => {
   return (req: Request, _res: Response, next: NextFunction): void => {
     try {
       if (!req.user) {
